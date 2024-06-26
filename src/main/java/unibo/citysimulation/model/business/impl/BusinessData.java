6b1f9b7e617373248991195ba84459909860972b
Alessio <alessiobifulco24@gmail.com>
2024-06-26 15:57:35 +0200

    add(model): record BusinessData created to represent business data

---
+++ src/main/java/unibo/citysimulation/model/business/impl/BusinessData.java
@@ -0,0 +1,21 @@
+package unibo.citysimulation.model.business.impl;
+
+import java.time.LocalTime;
+import java.util.List;
+
+import unibo.citysimulation.model.zone.Zone;
+import unibo.citysimulation.utilities.Pair;
+
+public record BusinessData(
+int id, 
+List<Employee> employees, 
+LocalTime openingTime, 
+LocalTime closingTime, 
+double revenue, 
+int maxEmployees, 
+Pair<Integer, Integer> position, 
+int minAge, 
+int maxAge, 
+int maxTardiness, 
+Zone zone) {
+}
